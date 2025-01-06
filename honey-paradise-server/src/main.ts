e9706e19fc540f5cbc976d4fb839faae080758b1
Svat-dev <svatoslavgetun@gmail.com>
2025-01-06 23:31:55 +0300

    Prisma Added

--- honey-paradise-server/src/main.ts
+++ honey-paradise-server/src/main.ts
@@ -1,9 +1,23 @@
+import { ConfigService } from "@nestjs/config/dist/config.service";
 import { NestFactory } from "@nestjs/core";
+import cookieParser from "cookie-parser";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	await app.listen(4000);
+	const config = app.get(ConfigService);
+
+	app.setGlobalPrefix("api");
+
+	app.use(cookieParser(config.getOrThrow<string>("COOKIES_SECRET")));
+
+	app.enableCors({
+		origin: [config.getOrThrow<string>("CLIENT_URL"), "https://accounts.google.com"],
+		credentials: true,
+		exposedHeaders: ["set-cookie"],
+	});
+
+	await app.listen(config.getOrThrow<number>("SERVER_PORT"));
 }
 
 bootstrap();
--- honey-paradise-server/src/prisma/prisma.module.ts
+++ honey-paradise-server/src/prisma/prisma.module.ts
@@ -1,7 +1,10 @@
-import { Module } from "@nestjs/common";
+import { Global } from "@nestjs/common/decorators/modules/global.decorator";
+import { Module } from "@nestjs/common/decorators/modules/module.decorator";
 import { PrismaService } from "./prisma.service";
 
+@Global()
 @Module({
 	providers: [PrismaService],
+	exports: [PrismaService],
 })
 export class PrismaModule {}
