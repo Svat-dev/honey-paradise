b7661b9eae0ed6337c1585ebdc81bc5ea4dd37c2
Svat-dev <svatoslavgetun@gmail.com>
2025-01-15 18:28:42 +0300

    Tailwind Config Edited

--- honey-paradise-client/tailwind.config.ts
+++ honey-paradise-client/tailwind.config.ts
@@ -1,18 +1,28 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: ["./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
+	darkMode: "media",
+	// prefix: "tw-",
+	theme: {
+		container: {
+			center: true,
+			padding: "2rem",
+			screens: {
+				"2xl": "1400px",
+			},
+		},
+		extend: {
+			colors: {
+				background: "var(--background)",
+				foreground: "var(--foreground)",
+			},
+		},
+		transitionDuration: {
+			DEFAULT: "300ms",
+			"250": "250ms",
+			"350": "350ms",
+		},
+	},
+	plugins: [],
 } satisfies Config;
