01e61ad74f67cf997c5a76e6327990549aede1ee
Svat-dev <svatoslavgetun@gmail.com>
2025-01-15 18:36:18 +0300

    Configs Edited

--- honey-paradise-client/next.config.ts
+++ honey-paradise-client/next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+	reactStrictMode: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "github.githubassets.com",
+			},
+			{
+				protocol: "https",
+				hostname: "fonts.gstatic.com",
+			},
+		],
+	},
 };
 
 export default nextConfig;
