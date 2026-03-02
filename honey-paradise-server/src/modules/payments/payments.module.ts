e20617e5d3a720bad6e89634e13fb153a174d434
Svat-dev <svatoslavgetun@gmail.com>
2026-03-02 23:28:06 +0300

    Added yookassa config

--- honey-paradise-server/src/modules/payments/payments.module.ts
+++ honey-paradise-server/src/modules/payments/payments.module.ts
@@ -1,9 +1,20 @@
 import { Module } from "@nestjs/common/decorators/modules/module.decorator"
+import { ConfigModule } from "@nestjs/config/dist/config.module"
+import { ConfigService } from "@nestjs/config/dist/config.service"
+import { YookassaModule } from "nestjs-yookassa/dist/yookassa.module"
+import { getYookassaConfig } from "src/core/config/yookassa.config"
 
 import { PaymentsController } from "./payments.controller"
 import { PaymentsService } from "./payments.service"
 
 @Module({
+	imports: [
+		YookassaModule.forRootAsync({
+			imports: [ConfigModule],
+			useFactory: getYookassaConfig,
+			inject: [ConfigService]
+		})
+	],
 	controllers: [PaymentsController],
 	providers: [PaymentsService]
 })
--- honey-paradise-server/src/modules/payments/payments.service.ts
+++ honey-paradise-server/src/modules/payments/payments.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from "@nestjs/common/decorators/core/injectable.decorator"
+import { YookassaService } from "nestjs-yookassa/dist/yookassa.service"
 
 @Injectable()
-export class PaymentsService {}
+export class PaymentsService {
+	constructor(private readonly yookassaService: YookassaService) {}
+}
